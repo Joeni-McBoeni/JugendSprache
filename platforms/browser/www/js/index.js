b59405a49c427ad670e0f65fac379d6b38696419
Jonas Wiesli 3i <jonas.wiesli@stud.kftg.ch>
2019-10-01 15:13:44 +0200

    add page 3

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,63 +1,65 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 // OnsenUI Page listener
- document.addEventListener('init', function(event) {
-   var page = event.target;
+document.addEventListener('init', function(event) {
+  var page = event.target;
 
-   if (page.id === 'page1') {
-     page.querySelector('#push-button-translator').onclick = function() {
-       document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Übersetzer'}});
-     };
-   } else if (page.id === 'page2') {
-     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-     page.querySelector('#push-button-confirm').onclick = function() {
-       document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
-     };
-   }
- });
+  if (page.id === 'page1') {
+    page.querySelector('#push-button-translator').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Übersetzer'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    page.querySelector('#push-button-confirm').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Resultat'}});
+    }
+  } else if (page.id === 'page3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  };
+});
 
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+  },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+    console.log('Received Event: ' + id);
+  }
 };
 
 app.initialize();
